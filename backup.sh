902f9a77ca12e7f18fc2c70fa451cafcb3a820c4
Valerio Martini <valerio.martini@gmail.com>
2022-01-17 17:36:44 +0100

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -6,15 +6,12 @@ echo ------------------------------------------------- Dell NETWORKING ---------
 echo ------------------------------------------------------------------------------------------------------
 
 # a git account and git running on the node is needed. https://docs.github.com/en 
-# change the name as it is your project now 
-
+# cd os10-config-backup/
+#change the name as your project needs, check EVPN multisite or EVPN DCI for further examples 
 ansible-playbook -i inventory copyconfig.yml
 git status
-# please add the RIGHT NAME AND EMAIL
-echo ---- > YUO NEED TO ADD YOUR ACCOUNT otherwise will remain local backup
 git config --global user.email "your@mail"
 git config --global user.name "your-name"
-
 git pull
 git add .
 git commit -a -m "standard update"
@@ -31,6 +28,6 @@ echo ------------------------------------------------- Dell NETWORKING ---------
 echo -----------------valerio.martini@gmail.com------------------------------------------------------------
 echo ------------------------------------------------------------------------------------------------------
 
-echo --- Please note on git auth changes August 2021 
-echo --- Single password not accepted anymore setup your token
-echo --- https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
+echo ---- please note on git auth changes August 2021 - single password not accepted anymore
+echo - setup your token
+echo - https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
